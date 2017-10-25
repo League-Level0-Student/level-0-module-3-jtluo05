b3ef0ad34edd5ad06fa7f7fd198f9565bdcbb246
100297 <league@WTS7.attlocal.net>
2017-10-24 19:47:50 -0700

    fizz buzz, SpudCounter, oddly loopy

---
+++ src/checkpoint/SpudCounter.java
@@ -0,0 +1,19 @@
+package checkpoint;
+
+public class SpudCounter {
+	public static void main(String[] args) {
+		for (int i = 1; i < 8; i++) {
+			if (i == 4) {
+				System.out.println(i);
+			}
+
+			else {
+
+				System.out.print(i + " potato, ");
+
+			}
+
+		}
+		System.out.print("more");
+	}
+}
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,15 +5,18 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+String time=JOptionPane.showInputDialog("How many shiny objects do you have");
 		// 3. Play the sound that many times
+int times=Integer.parseInt(time);
 
+playMisterZee();
 		// 1. Call the method below
 
 	}
